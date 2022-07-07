813c1127fd0c743f86c486b34060f45c3a065a58
Ryknot <seg_jes@hotmail.com>
2022-07-07 15:41:11 +0200

    deploiement: add migration

--- migrations/Version20220707133808.php
+++ migrations/Version20220707133808.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220707132719 extends AbstractMigration
+final class Version20220707133808 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -19,7 +19,7 @@ final class Version20220707132719 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql("INSERT INTO `user` (`id`, `email`, `roles`, `password`, `pseudo`, `date_register`, `date_connection`, `validated`, `code_validation`, `last_code_validation`, `active`) VALUES (NULL, 'seg_jes@hotmail.com', '[\"ROLE_ADMIN\"]', 'admin', 'Ryknot', '2022-07-07 15:22:11', '2022-07-07 15:22:11', '1', '999999', NULL, '1')");
+        $this->addSql("DELETE FROM `user` WHERE `user`.`email` = 'seg_jes@hotmail.com'");
     }
 
     public function down(Schema $schema): void
