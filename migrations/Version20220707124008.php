8f3d37736fafcf03aa978e3f65a53db2846389fe
Ryknot <seg_jes@hotmail.com>
2022-07-07 14:40:32 +0200

    deploiement: add migration

--- migrations/Version20220707124008.php
+++ migrations/Version20220707124008.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220707123013 extends AbstractMigration
+final class Version20220707124008 extends AbstractMigration
 {
     public function getDescription(): string
     {
