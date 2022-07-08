7dfbf64fd70e177f951eadd24b4f0835764ebe66
Ryknot <seg_jes@hotmail.com>
2022-07-08 11:33:38 +0200

    correctif: ajax ressourceUpdate

--- public/ajax/ressourceUpdateAjax.php
+++ public/ajax/ressourceUpdateAjax.php
@@ -4,7 +4,7 @@
 if (isset($_POST['id']) && isset($_POST['valeurGlissante']))
 {
     /* connection à la base de donnée mySQL (adresse hôte, nom bdd , identifiant , mdp , aide affichage erreur) */
-    $bdd = new PDO('mysql:host=localhost;dbname=projetperso','root','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ));
+    $bdd = new PDO('mysql:host=https://monclasseurjdr.herokuapp.com;dbname=x51fzdnz1odnbtsl','fkd9br3wzh38k1pk','', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION ));
 
     /* appel des informations ( selection des champs, du tableau 'ressource') */
     $requete = $bdd->prepare('UPDATE ressource SET valeur_glissante = ?  WHERE id = ?');
